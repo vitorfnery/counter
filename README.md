d37a465e1988968bfeaca0c7d52c53af275e9da8
Vítor F. Nery <vitorfelipenery@gmail.com>
2022-10-07 14:09:36 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Counter ➕ ➖ ➕ ➖ ➕ ➖
+
+![preview](./.github/preview.gif)
+
+> # Description / Descrição
+
+A counter controlled by plus and minus buttons. It also includes a reset button.
+
+[🔗 Click here to access / Clique aqui para acessar](https://counter-vitorfnery.netlify.app/)
+
+## 🛠️ Technologies / Tecnologias
+
+- JavaScript
+- HTML
+- CSS
+- Git and Github
+
+## 📣 Contact / Contato
+
+vitorfelipenery@gmail.com
---
+++ index.js
@@ -0,0 +1,19 @@
+"use strict";
+
+let num = 0;
+
+const value = document.querySelector(".value");
+const btns = document.querySelectorAll(".btn");
+
+const btnsCounter = btns.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    const styles = e.currentTarget.classList;
+    if (styles.contains("btn-danger")) num--;
+    if (styles.contains("btn-success")) num++;
+    if (styles.contains("btn-secondary")) num = 0;
+    value.textContent = num;
+    if (num > 0) value.style.color = "green";
+    if (num < 0) value.style.color = "red";
+    if (num === 0) value.style.color = "black";
+  });
+});
